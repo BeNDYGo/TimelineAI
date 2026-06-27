ff214659cadac032514f2e80ba5d564ff2fd862e
BeNDYGo <palexascha@gmail.com>
2026-06-27 19:42:27 +0300

    some changes

--- BaseAI/API-AI.py
+++ BaseAI/API-AI.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Dict
 from AI import chat
 
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ChatRequest(BaseModel):
